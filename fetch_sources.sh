69e94f3a43422f10f07a4e0630ff413be1354669
paradust7 <102263465+paradust7@users.noreply.github.com>
2026-07-31 09:13:45 -0400

    Bring in fix for curl proxy

--- fetch_sources.sh
+++ fetch_sources.sh
@@ -24,4 +24,4 @@ getrepo minetest_game "https://github.com/luanti-org/minetest_game.git" 052ccb09
 
 # These repos are part of the fork
 getrepo webshims "https://github.com/paradust7/webshims.git" 0767fdedd87f61a28a34f6444b669caf563a9fd5
-getrepo luanti "https://github.com/paradust7/luanti.git" 132c5ac52bb66d5f358320b9ab0cdb00891ff1ff
+getrepo luanti "https://github.com/paradust7/luanti.git" 56e45fa4e7c02c7f17fd9b5d6e1dcf145c543220
